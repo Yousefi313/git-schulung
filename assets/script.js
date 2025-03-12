0d2d21c9a440bac959f22be056f80f90a3d31c79
JDi-Coding <148185445+JDi-Coding@users.noreply.github.com>
2025-03-12 13:07:29 +0100

    Removed script.js

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-/*
-Hier könnte ihr eure Javascript logic ausführen 
-
-###########################################################
-Achtung diese Datei ist Global für alle Seiten verfügbar!!!
-###########################################################
- */
-
-console.log("HALLO WELT")
